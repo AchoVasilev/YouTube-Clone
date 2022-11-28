fdb29da98fdfd13557bff200003cb92ec34dd158
Angel Vasilev <angavasilev@gmail.com>
2022-11-28 06:10:44 +0200

    add header component

--- client/youtube-clone-client/src/app/app.module.ts
+++ client/youtube-clone-client/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxFileDropModule } from 'ngx-file-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadVideoComponent
+    UploadVideoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +22,9 @@ import { UploadVideoComponent } from './upload-video/upload-video.component';
     FormsModule,
     HttpClientModule,
     NgxFileDropModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
