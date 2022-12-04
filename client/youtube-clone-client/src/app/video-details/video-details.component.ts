e53f82f7ae0eb3914b1dd33baefc2b5d50573ab5
Angel Vasilev <angavasilev@gmail.com>
2022-12-04 08:58:44 +0200

    add video-details html

--- client/youtube-clone-client/src/app/video-details/video-details.component.ts
+++ client/youtube-clone-client/src/app/video-details/video-details.component.ts
@@ -11,6 +11,9 @@ export class VideoDetailsComponent {
   videoId: string = '';
   videoUrl: string = '';
   videoAvailable: boolean = false;
+  videoTitle: string = '';
+  videoDescription: string = '';
+  tags: string[] = [];
 
   constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService) {
     this.videoId = this.activatedRoute.snapshot.params['videoId'];
@@ -18,6 +21,10 @@ export class VideoDetailsComponent {
     this.videoService.getVideo(this.videoId)
       .subscribe(data => {
         this.videoUrl = data.videoUrl;
+        this.videoTitle = data.title;
+        this.videoDescription = data.description;
+        this.tags = data.tags;
+
         this.videoAvailable = true;
       });
   }
