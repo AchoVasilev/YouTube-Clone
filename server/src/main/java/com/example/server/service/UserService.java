40df193312928fa2f7fb545a75e40ec496b7df5f
Angel Vasilev <angavasilev@gmail.com>
2022-12-04 09:53:04 +0200

    add user registration and save user to the repository

--- server/src/main/java/com/example/server/service/UserService.java
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -1,6 +1,10 @@
 package com.example.server.service;
 
+import com.example.server.dto.UserInfoDto;
+import com.example.server.model.User;
 import com.example.server.repository.UserRepository;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,7 +35,21 @@ public class UserService {
                 .build();
 
         try {
-            httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            var responseString = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            var body = responseString.body();
+
+            var objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            var userInfo = objectMapper.readValue(body, UserInfoDto.class);
+
+            var user = new User();
+            user.setFirstName(userInfo.getGivenName());
+            user.setLastName(userInfo.getFamilyName());
+            user.setFullName(userInfo.getName());
+            user.setEmailAddress(userInfo.getEmail());
+            user.setSub(userInfo.getSub());
+
+            this.userRepository.save(user);
         } catch (IOException | InterruptedException exception) {
             throw new RuntimeException("Exception occurred while registering user.", exception);
         }
