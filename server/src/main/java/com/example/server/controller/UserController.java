2f27c2ad51b80aaf734e36f2d72b06c10034e4e4
Angel Vasilev <angavasilev@gmail.com>
2022-12-04 12:29:18 +0200

    add comments to video and display all comments

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.server.controller;
 
 import com.example.server.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
     public String register(Authentication authentication) {
         var jwt = (Jwt) authentication.getPrincipal();
 
@@ -23,11 +25,13 @@ public class UserController {
     }
 
     @PostMapping("/{userId}/subscribe")
+    @ResponseStatus(HttpStatus.OK)
     public void subscribeToUser(@PathVariable("userId") String userId) {
         this.userService.subscribeToUser(userId);
     }
 
     @PostMapping("/{userId}/unsubscribe")
+    @ResponseStatus(HttpStatus.OK)
     public void unsubscribeFromUser(@PathVariable("userId") String userId) {
         this.userService.unsubscribeFromUser(userId);
     }
